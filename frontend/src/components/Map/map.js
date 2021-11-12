8d374ebd33676f668b3b813169b624bfe7cc821d
Tanay Naik <macbookair@Tanays-MacBook-Air.local>
2021-11-12 18:50:52 +0530

    Update profile

--- frontend/src/components/Map/map.js
+++ frontend/src/components/Map/map.js
@@ -19,13 +19,6 @@ const DEFUALT_LONGITUDE = 72.8777;
 
 function MyMap(props) {
   const pickUp = props.pU;
-  // const drop = props.d;
-  // const setDrop = props.sD;
-  const isMapInit = props.mI;
-  const setPickUp = props.sPU;
-  const coords = props.coords;
-  const { user } = useContext(GlobalContext);
-  const [userData, setUserData] = user;
 
   const [map, setMap] = useState();
 
@@ -33,21 +26,10 @@ function MyMap(props) {
     setMap(map);
   };
 
-  const longitude = DEFUALT_LONGITUDE;
-  const latitude = DEFAULT_LATITUDE;
+  const longitude = pickUp ? pickUp.lng : DEFUALT_LONGITUDE;
+  const latitude = pickUp ? pickUp.lat : DEFAULT_LATITUDE;
 
   console.log("LAT", pickUp);
-
-  const customer = new Icon({
-    iconUrl:
-      "https://icon-library.com/images/google-maps-person-icon/google-maps-person-icon-6.jpg",
-    iconSize: [50, 50],
-  });
-  const driver = new Icon({
-    iconUrl:
-      "https://cdn2.iconfinder.com/data/icons/top-view-cars-1/50/49-512.png",
-    iconSize: [50, 50],
-  });
 
   return (
     <React.Fragment>
@@ -63,67 +45,13 @@ function MyMap(props) {
         />
         <ZoomControl position="bottomright" />
 
-        {/* {!isDriver &&  */}
-        <React.Fragment>
-          {/* Customer */}
-          <CircleMarker center={[pickUp.lat, pickUp.lng]} radius={100} />
-          {/* Driver(s) */}
-          {/* <Marker position={[pickUp.lat+0.03, pickUp.lng+0.03]} icon={driver} />
-        <Marker position={[pickUp.lat-0.01, pickUp.lng-0.01]} icon={driver} /> */}
-        </React.Fragment>
-        {/* } */}
-
-        {/* pickUp/Drop */}
-        {/* {!isDriver && */}
-        <React.Fragment>
-          <ReactLeafletSearch
-            className="custom-style"
-            position="topleft"
-            provider="OpenStreetMap"
-            openSearchOnLoad={true}
-            inputPlaceholder="Enter Location"
-            closeResultsOnClick={true}
-            zoom={14}
-            search={pickUp.info}
-            showPopup={false}
-            showMarker={true}
-            markerIcon={customer}
-            onChange={(e) => {
-              setPickUp({
-                address: e.info,
-                lat: e.latLng.lat,
-                lng: e.latLng.lng,
-              });
-              // setStep((prev) => prev+1);
-            }}
-          />
-          {/* <ReactLeafletSearch 
-        position="topright" 
-        provider="OpenStreetMap" 
-        openSearchOnLoad={true} 
-        inputPlaceholder="Enter Destination" 
-        closeResultsOnClick={true}
-        zoom={12}
-        showPopup={false}
-        onChange={(e) => {
-          setDrop({address: e.info, lat: e.latLng.lat, lng: e.latLng.lng});
-          // setStep((prev) => prev+1);
-        }}
-        /> */}
-        </React.Fragment>
-        {/* } */}
-
         {/* {isDriver &&  */}
         <React.Fragment>
-          <CircleMarker center={[pickUp.lat, pickUp.lng]} radius={100} />
+          <CircleMarker center={[latitude, longitude]} radius={70} />
           {/* <Marker position={[pickUp.lat, pickUp.lng]}/> */}
           {/* <Marker position={[drop.lat, drop.lng]} /> */}
         </React.Fragment>
         {/* } */}
-
-        {/* Path */}
-        {/* {isMapInit*isDriver && 
-      <Routing map={map} pickUp={pickUp} drop={drop} />} */}
       </Map>
     </React.Fragment>
   );
