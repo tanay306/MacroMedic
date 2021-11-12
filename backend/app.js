e40fe3709a0d98ddd3d4f9bca87ace2a60d74678
unknown <tanaygandhi306@gmail.com>
2021-11-12 19:36:31 +0530

    updates

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors({ credentials: true }));
 app.use(cors());
 
@@ -33,96 +34,96 @@ app.use(
         context: { req },
         graphiql: true,
       };
-    })
-  );
+  })
+);
 
-  let server = require( 'http' ).Server( app );
-  let io = require( 'socket.io' )( server );
-  let stream = require( './ws/stream' );
-  let path = require( 'path' );
-  let favicon = require( 'serve-favicon' );
-  
-  app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
-  app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+let server = require( 'http' ).Server( app );
+let io = require( 'socket.io' )( server );
+let stream = require( './ws/stream' );
+let path = require( 'path' );
+let favicon = require( 'serve-favicon' );
 
-  const attendedMeet = async (id) => {
-    try {
-      let appointment = await Appointment.findById(id);
-      if (appointment) {
-        appointment.status = "Visited";
-        await appointment.save();
-      } else {
-        console.log('n');
-      }
-    }  catch (err) {
-      console.log(err);
-      throw err;
-    }
-  }
-  
-  app.get( '/', ( req, res ) => {
-      let id = req.originalUrl.substring(7);
-      console.log(id);
-      let date = new Date();
-      date.setHours(date.getHours() + 1);
-      const job = schedule.scheduleJob(date, function(){
-        attendedMeet(id);
-      });
-      res.sendFile( __dirname + '/index.html' );
-  } );
-  
-  io.of( '/stream' ).on( 'connection', stream );
+app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
+app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
 
-  const multer = require('multer');
-  let DIR = './uploads/';
-  const { uuid } = require('uuidv4');
-  const User = require('./models/user.js');
-  const Appointment = require('./models/appointment.js')
-  
-  const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, DIR);
-    },
-    filename: (req, file, cb) => {
-      const fileName = file.originalname.toLowerCase().split(' ').join('-');
-      cb(null, uuid() + '-' + fileName);
-    },
-  });
-  //multer middleware
-  let upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, cb) {
-      checkFileType(file, cb);
-    },
-  });
-  function checkFileType(file, cb) {
-    const filetypes = /pdf|jpg|jpeg/;
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
-    if (mimetype && extname) {
-      return cb(null, true);
+const attendedMeet = async (id) => {
+  try {
+    let appointment = await Appointment.findById(id);
+    if (appointment) {
+      appointment.status = "Visited";
+      await appointment.save();
     } else {
-      cb('Error: Files Only!');
+      console.log('n');
     }
+  }  catch (err) {
+    console.log(err);
+    throw err;
   }
-  
-  app.post('/uploadDoc', upload.single('doc'), async (req, res) => {
-    const url = req.protocol + '://' + req.get('host');
-    const { id } = req.body;
-    const appointment = await Appointment.findById(id);
-    if(appointment) {
-      appointment.report = `/${req.file.path}`;
-      const resp = await appointment.save();
-      if(resp) {
-        res.send('Successfully uploaded pdf');
-        console.log('success');
-        } else {
-          res.send("Some err has occured!!");
-        }
-    }   
-  });    
+}
 
-app.post('/uploadImage', upload.single('doc'), async (req, res) => {
+app.get( '/', ( req, res ) => {
+    let id = req.originalUrl.substring(7);
+    console.log(id);
+    let date = new Date();
+    date.setHours(date.getHours() + 1);
+    const job = schedule.scheduleJob(date, function(){
+      attendedMeet(id);
+    });
+    res.sendFile( __dirname + '/index.html' );
+} );
+
+io.of( '/stream' ).on( 'connection', stream );
+
+const multer = require('multer');
+let DIR = './uploads/';
+const { uuid } = require('uuidv4');
+const User = require('./models/user.js');
+const Appointment = require('./models/appointment.js')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, DIR);
+  },
+  filename: (req, file, cb) => {
+    const fileName = file.originalname.toLowerCase().split(' ').join('-');
+    cb(null, uuid() + '-' + fileName);
+  },
+});
+//multer middleware
+let upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+function checkFileType(file, cb) {
+  const filetypes = /pdf|jpg|jpeg/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Files Only!');
+  }
+}
+
+app.post('/uploadDoc', upload.single('doc'), async (req, res) => {
+  const url = req.protocol + '://' + req.get('host');
+  const { id } = req.body;
+  const appointment = await Appointment.findById(id);
+  if(appointment) {
+    appointment.report = `/${req.file.path}`;
+    const resp = await appointment.save();
+    if(resp) {
+      res.send('Successfully uploaded pdf');
+      console.log('success');
+      } else {
+        res.send("Some err has occured!!");
+      }
+  }   
+});    
+
+app.post('/uploadImage', upload.single('doc'),  async (req, res) => {
   const {id} = req.body;
   console.log(id);
   const user = await User.findById(id);
@@ -135,7 +136,7 @@ app.post('/uploadImage', upload.single('doc'), async (req, res) => {
       res.send("Some err has occured!!");
     }
   }
-})
+});
 
 const PORT = process.env.PORT || 5000;
 
